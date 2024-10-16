ede40f4d81cff0bc910b92302bcb34a530ec4f72
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-10-16 18:45:47 +0530

    Initial Commit

---
+++ NetworkingDemoApp/NetworkingDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingDemoAppApp.swift
+//  NetworkingDemoApp
+//
+//  Created by Thushini Abeysuriya on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
